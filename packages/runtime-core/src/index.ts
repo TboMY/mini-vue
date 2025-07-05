f1024bdaccc504f66947eba9bfadae2e57c9ef2a
cj <2322507003@qq.com>
2025-07-05 16:58:49 +0800

    add 搭建runtime-core的monorepo框架

---
+++ packages/runtime-core/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @Author: cj
+ * @Date: 2025/7/5 下午4:14
+ *
+ */
+
+export function createRenderer(createRendererOptions){
+
+}
--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -9,19 +9,16 @@
 // runtime-dom 模块，主要是负责提供操作dom的api，（类似jq）
 export * from '@mini-vue/shared'
 export * from '@mini-vue/shared'
+export * from "@mini-vue/runtime-core"
 
 import {nodeOps} from "./nodeOps";
 import {patchProp} from "./patchProps";
+import {createRenderer} from "@mini-vue/runtime-core"
 
-// 形成一个options对象, patchProps是用于处理dom的属性的, 比较复杂单独处理, 但也是
+// 形成一个options对象, patchProps是用于处理dom的属性的, 比较复杂单独处理, 但也是属于options
 const createRendererOptions = Object.assign({patchProp}, nodeOps)
 
-export {createRendererOptions}
-
-
-function createRenderer() {
-
-}
+const render = createRenderer(createRendererOptions)
 
 
 
