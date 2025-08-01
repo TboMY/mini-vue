6936cf288a071c7d39c62b013e0d10b35124ee49
cj <2322507003@qq.com>
2025-08-01 10:45:42 +0800

    setup函数v0.1

--- packages/runtime-core/src/component.ts
+++ packages/runtime-core/src/component.ts
@@ -1,5 +1,5 @@
 import {hasOwn, isFunction} from "@mini-vue/shared";
-import {reactive} from "@mini-vue/reactivity";
+import {proxyRefs, reactive} from "@mini-vue/reactivity";
 
 export function createComponentInstance(vnode) {
     // type才是组件对象, 因为创建vnode的时候, 是比如 h(VueComponent), VueComponent才是object
@@ -7,6 +7,8 @@ export function createComponentInstance(vnode) {
 
     const instance = {
         data: null,
+        props: null,
+        attrs: null,
         render: null,
         update: null,
         vnode,
@@ -14,13 +16,14 @@ export function createComponentInstance(vnode) {
         isMounted: false, // 标识符, 如果render里面修改了state, 导致指数爆炸级别的递归次数
         propsOptions, // 定义组件时的props
         proxy: null, // 这个组件实例的代理对象, 便于开发直接访问props,data里面的数据
+        setupState: null // setup函数返回值为对象时
     }
     return instance
 }
 
 export function setupComponentInstance(instance) {
     const {vnode} = instance
-    const {data, render} = vnode.type
+    const {data, render, setup} = vnode.type
 
     // 在组件中可以通过代理对象直接访问data,props,attrs等,
     // 而不用 this.props.age, 方便开发者
@@ -32,6 +35,21 @@ export function setupComponentInstance(instance) {
     const state = reactive(isFunction(data) ? data.call(proxy) : (data || {}))
     instance.data = state
     instance.render = render
+
+    // setup返回值可能是一个fn或obj; 是fn就代替render的作用(优先级高于单独的render), 是obj的作用和data类似(比如可以在render中使用),但不合并到data里面
+    if (setup) {
+        // context为 props, emits, attrs, slots,exposed
+        const context = {
+            props: instance.props,
+            attrs: instance.attrs
+        }
+        const setupResult = setup(context)
+        if (isFunction(setupResult)) {
+            instance.render = setupResult
+        } else {
+            instance.setupState = proxyRefs(setupResult)
+        }
+    }
 }
 
 const initProps = (instance, vNodeProps) => {
@@ -68,11 +86,13 @@ const publicProperty = {
 const instanceProxyHandler = {
     get(target, key, receiver) {
         // debugger
-        const {data, props,} = target
+        const {data, props, setupState} = target
         if (data && hasOwn(data, key)) {
             return Reflect.get(data, key, receiver)
         } else if (props && hasOwn(props, key)) {
             return Reflect.get(props, key, receiver)
+        } else if (setupState && hasOwn(setupState, key)) {
+            return Reflect.get(setupState, key, receiver)
         }
         // instance上面会挂载一些公开的, 但是不准修改的属性, 比如($attrs, $slot)
         const getter = publicProperty[key]
@@ -80,7 +100,7 @@ const instanceProxyHandler = {
     },
     set(target, key, val, receiver) {
         // debugger
-        const {data, props,} = target
+        const {data, props, setupState} = target
         if (data && hasOwn(data, key)) {
             // 这里不能使用Reflect.set, 因为这里使用set会触发在reactive的proxyHandlers中的set，
             // 在那个方法中也会用到Reflect.set, 导致newvalue没有被更新到data中
@@ -91,6 +111,8 @@ const instanceProxyHandler = {
             console.warn('props应该是只读的')
             // return Reflect.set(props, key, val, receiver)
             props[key] = val
+        } else if (setupState && hasOwn(setupState, key)) {
+            setupState[key] = val
         }
         return true
     }
--- packages/runtime-core/src/index.ts
+++ packages/runtime-core/src/index.ts
@@ -3,6 +3,7 @@
  * @Date: 2025/7/5 下午4:14
  *
  */
+export * from '@mini-vue/reactivity'
 export * from './render'
 export * from './h'
 export {createVNode} from "./vNode";
